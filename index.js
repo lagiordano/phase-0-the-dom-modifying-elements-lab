fd747700be32fd3770ec665c9e9b304b27d2cc1a
Lauren Giordano <laurengiordano94@gmail.com>
2023-04-03 13:40:04 +1000

    done with lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.append(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = ("Lauren is the champion");
